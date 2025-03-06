97aa9b3160bfe1f5af1e59e4354ae17d5efcd0b5
sageMuneng <genematthewaban10@gmail.com>
2025-03-06 11:59:41 +0800

    Dynamic Profile Card DONE
    Working To-Do DONE

--- 33 Dynamic Profile Card Editor/script.js
+++ 33 Dynamic Profile Card Editor/script.js
@@ -1,51 +1,12 @@
 function updateName(thisID, targetID){
-    let targetElement = document.getElementById(thisID);
-    let sourceValue = document.getElementById(targetID).value;
-    
-    if (sourceValue) {
-        targetElement.innerText = sourceValue;
-        displayMessage(true);
-    } else {
-        displayMessage(false);
-    }
+    document.getElementById(thisID).innerText = document.getElementById(targetID).value;
 }
 
 
 function updateProfile(){
-    if(document.getElementById("update_img_url").value){
-        document.getElementById("user_profile").src = document.getElementById("update_img_url").value;
-        displayMessage(true);
-    } else {
-        displayMessage(false);
-    }
+    document.getElementById("user_profile").src = document.getElementById("update_img_url").value;
 }
 
 function updateBG(){
-    if(document.getElementById("change_bg").value){
-        document.getElementById("profile_card").style.backgroundColor = document.getElementById("change_bg").value;
-        displayMessage(true);
-    } else {
-        displayMessage(false);
-    }
-}
-
-function displayMessage(status) {
-    console.log(status)
-    if(status){
-        document.getElementById('feedback').innerText = "Changes Applied";
-        document.getElementById('feedback').style.display = "block";
-        document.getElementById('feedback').style.backgroundColor = "greenyellow";
-        setTimeout(() => {
-            document.getElementById('feedback').innerText = "";
-            document.getElementById('feedback').style.display = "none";
-        }, 3000); 
-    }else{
-        document.getElementById('feedback').innerText = "Failed to apply";
-        document.getElementById('feedback').style.display = "block";
-        document.getElementById('feedback').style.backgroundColor = "red";
-        setTimeout(() => {
-            document.getElementById('feedback').innerText = "";
-            document.getElementById('feedback').style.display = "none";
-        }, 3000); 
-    }
+    document.getElementById("profile_card").style.backgroundColor = document.getElementById("change_bg").value;
 }
--- 34 Notification/script.js
+++ 34 Notification/script.js
@@ -1,51 +1,23 @@
-function updateName(thisID, targetID){
-    let targetElement = document.getElementById(thisID);
-    let sourceValue = document.getElementById(targetID).value;
-    
-    if (sourceValue) {
-        targetElement.innerText = sourceValue;
-        displayMessage(true);
-    } else {
-        displayMessage(false);
+function showNotif(){
+    let notif_container = document.getElementById("notif_container_list");
+    let newNotifList = document.createElement('li');
+    let closeButton = document.createElement('button')
+
+    newNotifList.innerHTML = `"You have a new message!"`
+    notif_container.appendChild(newNotifList);
+    newNotifList.appendChild(closeButton);
+    closeButton.innerText = ` X `
+    closeButton.onclick = () => removeNotif(newNotifList);
+    setTimeout(() => {
+        removeNotif(newNotifList);
+    }, 5000);
+}
+
+function removeNotif(removeThis) {
+    let notif_container = document.getElementById("notif_container_list");
+    if(removeThis){
+        notif_container.removeChild(removeThis);
     }
 }
 
 
-function updateProfile(){
-    if(document.getElementById("update_img_url").value){
-        document.getElementById("user_profile").src = document.getElementById("update_img_url").value;
-        displayMessage(true);
-    } else {
-        displayMessage(false);
-    }
-}
-
-function updateBG(){
-    if(document.getElementById("change_bg").value){
-        document.getElementById("profile_card").style.backgroundColor = document.getElementById("change_bg").value;
-        displayMessage(true);
-    } else {
-        displayMessage(false);
-    }
-}
-
-function displayMessage(status) {
-    console.log(status)
-    if(status){
-        document.getElementById('feedback').innerText = "Changes Applied";
-        document.getElementById('feedback').style.display = "block";
-        document.getElementById('feedback').style.backgroundColor = "greenyellow";
-        setTimeout(() => {
-            document.getElementById('feedback').innerText = "";
-            document.getElementById('feedback').style.display = "none";
-        }, 3000); 
-    }else{
-        document.getElementById('feedback').innerText = "Failed to apply";
-        document.getElementById('feedback').style.display = "block";
-        document.getElementById('feedback').style.backgroundColor = "red";
-        setTimeout(() => {
-            document.getElementById('feedback').innerText = "";
-            document.getElementById('feedback').style.display = "none";
-        }, 3000); 
-    }
-}
