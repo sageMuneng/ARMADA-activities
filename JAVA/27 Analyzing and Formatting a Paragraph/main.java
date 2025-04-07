9235934ab9f750f60561c34da6402c3449d861c9
sageMuneng <genematthewaban10@gmail.com>
2025-04-07 14:54:55 +0800

    Analyzing and Formatting a Paragraph

--- JAVA/27 Analyzing and Formatting a Paragraph/main.java
+++ JAVA/27 Analyzing and Formatting a Paragraph/main.java
@@ -2,6 +2,20 @@ import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         
+        System.out.println("Enter a paragraph: ");
+        String paragraph = scanner.nextLine().trim();
+        String trimmed_paragraph = paragraph;
+
+        String one_spaces = trimmed_paragraph.replaceAll("\\s+", " ");
+
+        String[] words = one_spaces.split(" ");
+
+        System.out.println("Word Count: " + words.length);
+
+        String replaced_word = one_spaces.replaceAll("Java", "Python");
+        System.out.println("Modified Paragraph: " + replaced_word);
+        scanner.close();
     }
 }
