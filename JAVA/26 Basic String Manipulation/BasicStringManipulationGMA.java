d085cf34f21fddaad731606108348a812140708c
sageMuneng <genematthewaban10@gmail.com>
2025-04-07 14:32:08 +0800

    Basic String Manipulation DONE

---
+++ JAVA/26 Basic String Manipulation/BasicStringManipulationGMA.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class BasicStringManipulationGMA{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your full name: ");
+        
+        String full_name = scanner.nextLine();
+        
+        System.out.println("Uppercase Name: " + full_name.toUpperCase());
+
+        String full_name_length = full_name.replace(" ", "");
+        System.out.println("Total Characters: " + full_name_length.length());
+
+
+        StringBuilder intitials = new StringBuilder();
+        for (int i = 0; i < full_name_length.length(); i++) {
+            if(full_name_length.charAt(i) == full_name_length.toUpperCase().charAt(i)){
+                intitials.append(full_name_length.charAt(i) + ".");
+            }
+        }
+
+        System.out.println("Initials: " + intitials.toString());
+    }
+}
