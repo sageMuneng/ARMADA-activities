e8a4c477d2c0216a35c3894309b13e4b31efc16f
sageMuneng <genematthewaban10@gmail.com>
2025-05-21 23:35:56 +0800

    hello jsx activity DONE

---
+++ REACTJS/HelloJSX/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+	const greeting = "Hello, JSX!";
+	const name = "React Developer's";
+	const year = 2025;
+
+	return (
+		<>
+			<h1>{greeting} Welcome to the {name} Journey in {year}</h1>
+		</>
+	)
+}
+
+export default App
