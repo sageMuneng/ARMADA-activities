6080fe44fa54c58bdb1ccae6f604a4cab5e722c5
sageMuneng <genematthewaban10@gmail.com>
2025-05-22 00:37:11 +0800

    Time based greetings DONE

---
+++ REACTJS/time-based-greetings/rendering-list/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+	const currentTime = new Date().getHours(); 
+	let greeting;
+	if(currentTime >= 5 && currentTime <= 11){
+		greeting = <h4>Good Morning!</h4>
+	}else if(currentTime >= 12 && currentTime <= 17){
+		greeting = <h4>Good Afternoon!</h4>
+	}else{
+		greeting = <h4>Good Evening!</h4>
+	}
+	return (
+		<>
+			<h1>{greeting}</h1>
+		</>
+	)
+}
+
+export default App
